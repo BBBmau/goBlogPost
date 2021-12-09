48f8854db0af51f754089c8f98785b328c1e1451
BBBmau <alvarez.mauriciotm@gmail.com>
2021-12-09 11:34:38 -0800

    Initial GoWebServer Code

--- goWebServers.go
+++ goWebServers.go
@@ -1,1 +1,15 @@
 package blogwebsite
+
+import (
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
