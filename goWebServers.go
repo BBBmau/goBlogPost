25c89f3611e915fff2f02431376fc5788122e09e
BBBmau <alvarez.mauriciotm@gmail.com>
2021-12-16 15:13:29 -0800

    Add Import Embed Package

--- goWebServers.go
+++ goWebServers.go
@@ -1,15 +1,33 @@
-package blogwebsite
+package main
 
 import (
-	"os"
+	"embed"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
+var tplFolder embed.FS
+
+//var blogPage []byte
+
+func indexHanlder(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles(tplFolder, "templates/blogPage.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	println("Embedded the blogPage File")
+	tmpl.Execute(w, nil)
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHanlder)
+
+	fmt.Printf("Starting server at port 8000\n")
+	err := http.ListenAndServe(":8000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
