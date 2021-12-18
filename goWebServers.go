a4e2bdbc02f6b604a3736e5a2bfc26842a132eaf
BBBmau <alvarez.mauriciotm@gmail.com>
2021-12-18 11:02:10 -0800

    Add Serve of assets for static pages

--- goWebServers.go
+++ goWebServers.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
-
-	"github.com/gorilla/mux"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,13 +41,33 @@ func blogHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("In blogHandler!")
 }
 
-func main() {
-	route := mux.NewRouter()
+//go:embed templates/*
+var tp1Folder embed.FS
 
-	route.HandleFunc("/", homeHandler)
+//go:embed assets/*
+var assets embed.FS
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, err := template.ParseFS(tp1Folder, "templates/homePage.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	println("Embedded homePage.html")
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	assetsFS := http.FileServer(http.FS(assets))
+	route := http.NewServeMux()
+	route.Handle("/assets/", assetsFS)
 	route.HandleFunc("/home", homeHandler)
 	route.HandleFunc("/about", aboutHandler)
 	route.HandleFunc("/blog", blogHandler)
+
+	//route.Handle("comma.png")
+
+	route.HandleFunc("/", indexHandler)
 	fmt.Printf("Server up!\n")
 
 	err := http.ListenAndServe(":8000", route)
